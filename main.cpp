235ec632289a656cca36ff45ddb7ccc2212aef4e
FRANKY WAHYU <frankywahyuprasetyo29@gmail.com>
2024-07-14 21:18:13 +0700

    Soal no 2 bagian A

--- main.cpp
+++ main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct Pegawai
+{
+    string nomorInduk;
+    string nama;
+    string alamat;
+    string noHp;
+    string jabatan;
+    string golongan;
+    float gaji;
+};
+
+float calculateSalary(const string& golongan) {
+    if (golongan == "D1") {
+        return 2500000;
+    }else if(golongan == "D2") {
+        return 200000;
+    }else if(golongan == "D3") {
+        return 1500000;
+    }else {
+        return 0;
+    }
+}
+
 
 int kalkulator() {
     const int threshold = 100000;
@@ -20,7 +48,45 @@ int kalkulator() {
 }
 
 int payroll() {
+    Pegawai pegawai;
 
+    getline(cin, pegawai.nomorInduk);//Tidak dihapus karena sudah menggunakan getline tetapi fungsi dari getline nya tidak bisa membuat baris baru,dan jika dihapus membuat NIP dan nama menjadi 1 line
+
+    cout << "Nip =";
+    getline(cin, pegawai.nomorInduk);
+
+    cout << "Nama =";
+    getline(cin, pegawai.nama);
+
+    cout << "Alamat =";
+    getline(cin, pegawai.alamat); 
+
+    cout << "No HP =";
+    getline(cin, pegawai.noHp);
+
+    cout << "Jabatan =";
+    getline(cin, pegawai.jabatan);
+
+    cout << "Golongan =";
+    getline(cin, pegawai.golongan);
+
+    
+    pegawai.gaji = calculateSalary(pegawai.golongan);
+
+   
+    if (pegawai.gaji == 0) {
+        cout << "Golongan tidak valid!" << endl;
+    } else {
+        cout << "\nGaji Bulanan" << endl;
+        cout << "Nip = " << pegawai.nomorInduk << endl;
+        cout << "Nama =" << pegawai.nama << endl;
+        cout << "Alamat = " << pegawai.alamat << endl;
+        cout << "No HP =" << pegawai.noHp << endl;
+        cout << "Jabatan =" << pegawai.jabatan << endl;
+        cout << "Golongan = " << pegawai.golongan << endl;
+        cout << fixed << setprecision(0);
+        cout << "Gaji = Rp" << pegawai.gaji << endl;
+    }
     return 0;
 }
 
